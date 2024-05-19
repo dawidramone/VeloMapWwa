076dbf864124ce367861aa05ff1c70dc3a180fa7
Dawid Czmyr <czmyrdawid@gmail.com>
2024-05-19 12:41:13 +0200

    Initial Commit

---
+++ VeloMapWwa/VeloMapWwaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeloMapWwaApp.swift
+//  VeloMapWwa
+//
+//  Created by Dawid Czmyr on 19/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct VeloMapWwaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
